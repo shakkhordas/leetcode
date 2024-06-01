1e5b92f4aa5a83b967d0034280ce527a340d622c
shakkhordas <shakkhordas117@gmail.com>
2024-06-01 23:40:03 +0600

    June 1, 2024 23:40

---
+++ Majority_Element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int res = nums[0], count = 1;
+        for (int i=1; i<nums.size(); i++) {
+            if (nums[i] == res)
+                count++;
+            else {
+                if (count == 0) {
+                    res = nums[i];
+                    count++;
+                }
+                else
+                    count--;
+            }
+        }
+        return res;
+    }
+};
